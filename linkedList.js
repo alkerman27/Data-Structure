9df8b391a76faeb387e8f3add1d6b6800467f2e7
Mark Canlas <m4rkd0tc0de@gmail.com>
2022-06-26 18:59:09 +0800

    adding linked list function

---
+++ linkedList.js
@@ -0,0 +1,90 @@
+// Create the below linked list:
+// myLinkedList = {
+//   head: {
+//     value: 10
+//     next: {
+//       value: 5
+//       next: {
+//         value: 16
+//         next: null
+//       }
+//     }
+//   }
+// };
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value,
+      next: null
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = []
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+    return array;
+  }
+  prepend(value) {
+    const newNode = {
+      value,
+      next: null
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  insert(index, value) {
+    if(index >= this.length) {
+      return this.append(value)
+    }
+    const newNode = {
+      value,
+      next: null
+    };
+    const leader = this.traverseToIndex(index - 1)
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer
+    this.length++;
+    return this;
+  }
+  remove(index) {
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this;
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+console.log(myLinkedList.append(5));
+myLinkedList.append(16);
+console.log(myLinkedList.prepend(7));
+console.log(myLinkedList.insert(200, 11));
